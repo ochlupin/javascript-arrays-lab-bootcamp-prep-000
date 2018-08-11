978c053b7e322ee02e2c27ed9b4d9690ec1e060c
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-11 14:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens]
 };
+
+
+//slices all elements in the array from index position 0 to second last position (.length-1) i.e. this translates slicing out all but the last element and returning the remainder as a new array
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+};
+
+// slices all the elements starting from index position 1 to the last postion i.e. this translates to slicing out the first element and returning the remainder as a new array
+function removeFirstKitten() {
+  return kittens.slice(1)
+};
